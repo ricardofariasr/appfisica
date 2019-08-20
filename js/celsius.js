341ad7fa98ebd371b0e26c4e24fb3f0a582b563f
ricardo <ricardo_farias@estudante.sc.senai.br>
2019-08-20 11:18:33 -0300

    caixa de texto no celsius ao invés de alert

--- js/celsius.js
+++ js/celsius.js
@@ -1,9 +1,9 @@
-var celsius = 0;
+function _onClick(){
 var kelvin = 0;
 var Fahrenheit = 0;
-celsius = prompt("digite a temperatura em graus celsius que você quer que converta:")
-celsius = parseInt(celsius)
+var celsius = document.getElementById("cel").value;
+celsius = parseInt(celsius);
 kelvin = celsius + 273;
 Fahrenheit = 1.8 * celsius + 32;
-
-document.write("A temperatura em graus celsius digitada, foi: " + celsius + "°.<br> Essa temperatura em graus Fahrenheit é aproximadamente: "+Fahrenheit+ "°.<br> E em Kelvin é aproximadamente: "+kelvin+ "°.")
+document.write("A temperatura em graus celsius digitada, foi: " + celsius + "°.<br> Essa temperatura em graus Fahrenheit é aproximadamente: "+Fahrenheit+ "°.<br> E em Kelvin é aproximadamente: "+kelvin+ "°.");
+}
